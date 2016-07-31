539557cb755913688a70c33a8862c8e7354fabe4
Jonathan Dann <nwjon122@gmail.coml>
2016-07-30 17:13:16 -0700

    xml config complete

---
+++ src/main/java/com/nwjon/sdata/BookService.java
@@ -0,0 +1,20 @@
+package com.nwjon.sdata;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by jondann on 7/29/16.
+ */
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository repo;
+
+    public void save(Book book){
+        this.repo.save(book);
+    }
+
+}
